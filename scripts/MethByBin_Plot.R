b5de96280fce5a35aeb60d2cfe8338b3f7cfea7c
Pedro Barros <pedro.pataias@gmail.com>
2019-10-28 16:55:13 +0000

    Update MethByBin_Plot.R

--- scripts/MethByBin_Plot.R
+++ scripts/MethByBin_Plot.R
@@ -21,7 +21,7 @@ main <- function() {
 "Required arguments: \n --input \t list of input files separated by ',' and no space\n --nbin \t number of bins to be considered for feature \n --label \t list of labels of thr input files separated by ',' and no space \n",
 "Optional arguments: \n --output \t default 'meth_output' \n --flank \t if flanking regions are to be considered \n --nbin_flank \t [required if --flank] number of bins of flanking region \n", 
 "--width \t figure width in cm, default 15 \n --height \t figure height in cm, default 10 \n --ylim \t upper limit of y axis. Default ajusted according to max values \n",
-"--group \t select group do plot. options: 'sample'[default] or 'coord'\n")
+"--group \t select group do plot. options: 'sample' or 'coord'\n")
     }
     if(args[i] == "--input"){
       meth <- args[i+1]
